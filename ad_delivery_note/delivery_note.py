3aa54dc33b0808b8e1e14b26e1596adda80e400d
La Jayuhni Yarsyah <lajayuhni@gmail.com>
2014-10-03 17:49:23 +0700

    Solving product move bug, on change product description does not required

--- ad_delivery_note/delivery_note.py
+++ ad_delivery_note/delivery_note.py
@@ -616,8 +616,42 @@ class stock_move(osv.osv):
     #     hasil=self.pool.get('product.product').browse(cr,uid,[prd])[0]
     #     uom=self.pool.get('product.template').browse(cr,uid,[prd])[0]
     #     return {'value':{ 'desc':hasil.name, 'product_qty':1, 'product_uom':uom.uom_id.id} }
-        
-    _defaults = {'no': 10}
+    def onchange_product_id(self, cr, uid, ids, prod_id=False, loc_id=False,
+                            loc_dest_id=False, partner_id=False):
+        """ On change of product id, if finds UoM, UoS, quantity and UoS quantity.
+        @param prod_id: Changed Product id
+        @param loc_id: Source location id
+        @param loc_dest_id: Destination location id
+        @param partner_id: Address id of partner
+        @return: Dictionary of values
+        """
+        if not prod_id:
+            return {}
+        user = self.pool.get('res.users').browse(cr, uid, uid)
+        lang = user and user.lang or False
+        if partner_id:
+            addr_rec = self.pool.get('res.partner').browse(cr, uid, partner_id)
+            if addr_rec:
+                lang = addr_rec and addr_rec.lang or False
+        ctx = {'lang': lang}
+
+        product = self.pool.get('product.product').browse(cr, uid, [prod_id], context=ctx)[0]
+        uos_id  = product.uos_id and product.uos_id.id or False
+        result = {
+            'product_uom': product.uom_id.id,
+            'product_uos': uos_id,
+            'product_qty': 1.00,
+            'product_uos_qty' : self.pool.get('stock.move').onchange_quantity(cr, uid, ids, prod_id, 1.00, product.uom_id.id, uos_id)['value']['product_uos_qty'],
+            'prodlot_id' : False,
+            'desc':product.name,
+        }
+        if not ids:
+            result['name'] = product.partner_ref
+        if loc_id:
+            result['location_id'] = loc_id
+        if loc_dest_id:
+            result['location_dest_id'] = loc_dest_id
+        return {'value': result}
    
 stock_move()
 
