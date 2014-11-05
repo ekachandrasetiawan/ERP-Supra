1f5461e92c53e433b47f3da35a4962c52043f5da
La Jayuhni Yarsyah <lajayuhni@gmail.com>
2014-11-05 09:55:12 +0700

    Fixing purcahse order split bom

--- ad_delivery_note/delivery_note.py
+++ ad_delivery_note/delivery_note.py
@@ -186,7 +186,7 @@ class stock_picking(osv.osv):
 		# print "CALLEDD"
 		# print "CALLED WRITE",ids
 		res = super(stock_picking,self).write(cr,uid,ids,vals,context)
-		self.cleanSetProductMove(cr,uid,ids,context)
+		# self.cleanSetProductMove(cr,uid,ids,context)
 		# print vals,"---INI",context
 		return res
 		
@@ -390,8 +390,96 @@ class stock_picking(osv.osv):
 	def setdraft(self,cr,uid,ids,context=None):
 		return self.write(cr,uid,ids,{'state':'draft'})
 
-
 stock_picking()
+
+class PurchaseOrder(osv.osv):
+	_inherit = "purchase.order"
+	# def _prepare_order_line_move(self, cr, uid, order, order_line, picking_id, context=None):
+ #        return {
+ #            'name': order_line.name or '',
+ #            'product_id': order_line.product_id.id,
+ #            'product_qty': order_line.product_qty,
+ #            'product_uos_qty': order_line.product_qty,
+ #            'product_uom': order_line.product_uom.id,
+ #            'product_uos': order_line.product_uom.id,
+ #            'date': self.date_to_datetime(cr, uid, order.date_order, context),
+ #            'date_expected': self.date_to_datetime(cr, uid, order_line.date_planned, context),
+ #            'location_id': order.partner_id.property_stock_supplier.id,
+ #            'location_dest_id': order.location_id.id,
+ #            'picking_id': picking_id,
+ #            'partner_id': order.dest_address_id.id or order.partner_id.id,
+ #            'move_dest_id': order_line.move_dest_id.id,
+ #            'state': 'draft',
+ #            'type':'in',
+ #            'purchase_line_id': order_line.id,
+ #            'company_id': order.company_id.id,
+ #            'price_unit': order_line.price_unit
+ #        }
+	def _create_pickings(self, cr, uid, order, order_lines, picking_id=False, context=None):
+		"""Creates pickings and appropriate stock moves for given order lines, then
+		confirms the moves, makes them available, and confirms the picking.
+
+		If ``picking_id`` is provided, the stock moves will be added to it, otherwise
+		a standard outgoing picking will be created to wrap the stock moves, as returned
+		by :meth:`~._prepare_order_picking`.
+
+		Modules that wish to customize the procurements or partition the stock moves over
+		multiple stock pickings may override this method and call ``super()`` with
+		different subsets of ``order_lines`` and/or preset ``picking_id`` values.
+
+		:param browse_record order: purchase order to which the order lines belong
+		:param list(browse_record) order_lines: purchase order line records for which picking
+												and moves should be created.
+		:param int picking_id: optional ID of a stock picking to which the created stock moves
+							   will be added. A new picking will be created if omitted.
+		:return: list of IDs of pickings used/created for the given order lines (usually just one)
+		"""
+		if not picking_id:
+			picking_id = self.pool.get('stock.picking').create(cr, uid, self._prepare_order_picking(cr, uid, order, context=context))
+		todo_moves = []
+		stock_move = self.pool.get('stock.move')
+		wf_service = netsvc.LocalService("workflow")
+		for order_line in order_lines:
+			if not order_line.product_id:
+				continue
+			if order_line.product_id.type in ('product', 'consu'):
+				if order_line.product_id.bom_ids:
+					if order_line.product_id.bom_ids[0].bom_lines:
+						for bom in order_line.product_id.bom_ids[0].bom_lines:
+							moveBom = {
+								'name': order_line.name or '',
+					            'product_id': bom.product_id.id,
+					            'product_qty': order_line.product_qty,
+					            'product_uos_qty': order_line.product_qty*bom.product_qty,
+					            'product_uom': bom.product_uom.id,
+					            'product_uos': bom.product_uom.id,
+					            'date': self.date_to_datetime(cr, uid, order.date_order, context),
+					            'date_expected': self.date_to_datetime(cr, uid, order_line.date_planned, context),
+					            'location_id': order.partner_id.property_stock_supplier.id,
+					            'location_dest_id': order.location_id.id,
+					            'picking_id': picking_id,
+					            'partner_id': order.dest_address_id.id or order.partner_id.id,
+					            'move_dest_id': order_line.move_dest_id.id,
+					            'state': 'draft',
+					            'type':'in',
+					            'purchase_line_id': order_line.id,
+					            'company_id': order.company_id.id,
+					            'price_unit': order_line.price_unit
+							}
+							move = stock_move.create(cr,uid,moveBom)
+							todo_moves.append(move)
+
+				else:
+					move = stock_move.create(cr, uid, self._prepare_order_line_move(cr, uid, order, order_line, picking_id, context=context))
+					if order_line.move_dest_id:
+						order_line.move_dest_id.write({'location_id': order.location_id.id})
+					todo_moves.append(move)
+		stock_move.action_confirm(cr, uid, todo_moves)
+		stock_move.force_assign(cr, uid, todo_moves)
+		wf_service.trg_validate(uid, 'stock.picking', picking_id, 'button_confirm', cr)
+		return [picking_id]
+	
+
 
 
 class move_set_data(osv.osv):
