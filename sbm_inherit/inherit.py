31be04bc690a0af488cc946904eed386eefe7e89
La Jayuhni Yarsyah <lajayuhni@gmail.com>
2014-12-12 11:13:55 +0700

    Change Class name stockPickingMinDate

--- sbm_inherit/inherit.py
+++ sbm_inherit/inherit.py
@@ -1172,7 +1172,7 @@ class stock_move(osv.osv):
 	}
 	
 # Fixing bug min_date on picking when null it will be filled timestamp
-class stock_picking(osv.osv):
+class stockPicking(osv.osv):
 	_inherit = 'stock.picking'
 	_columns = {
 		# 'min_date': fields.function(get_min_max_date, fnct_inv=_set_minimum_date, multi="min_max_date", store=True, type='datetime', string='Scheduled Time', select=1, help="Scheduled time for the shipment to be processed"),
