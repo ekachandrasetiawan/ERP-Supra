ad6d386c6492abfc0d4156dd6534843b156c72fd
Eka Chandra Setiawan <ekachandrasetiawan@yahoo.com>
2015-08-27 10:10:28 +0700

    Rounding Sales Order Line Dikurangi Discount Nominal

--- ad_discount/discount.py
+++ ad_discount/discount.py
@@ -129,8 +129,27 @@ class sale_order_line(osv.osv):
 
 	def _amount_line(self, cr, uid, ids, field_name, arg, context=None):
 
-		res = super(sale_order_line, self)._amount_line(cr,uid,ids, field_name, arg, context=None)
+		# res = super(sale_order_line, self)._amount_line(cr,uid,ids, field_name, arg, context=None)
 
+		tax_obj = self.pool.get('account.tax')
+		cur_obj = self.pool.get('res.currency')
+		
+		user_obj = self.pool.get('res.users')
+		currency_obj = self.pool.get('res.currency')
+		user = user_obj.browse(cr, uid, uid, context=context)
+
+		res = {}
+		if context is None:
+			context = {}
+		for line in self.browse(cr, uid, ids, context=context):
+			price = line.price_unit
+			taxes = tax_obj.compute_all(cr, uid, line.tax_id, price, line.product_uom_qty, line.product_id, line.order_id.partner_id)
+			cur = line.order_id.pricelist_id.currency_id
+			if (line.order_id.pricelist_id.currency_id.id==user.company_id.currency_id.id):
+				res[line.id] = cur_obj.round(cr, uid, cur, round(taxes['total']-line.discount_nominal))
+			else:
+				res[line.id] = cur_obj.round(cr, uid, cur, taxes['total']-line.discount_nominal)
+				
 		return res
 
 	_columns = {
