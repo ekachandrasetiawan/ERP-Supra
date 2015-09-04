36ca99bd91d4e39a4bd19131f8637687d49c0eba
La Jayuhni Yarsyah <lajayuhni@gmail.com>
2015-09-04 13:56:58 +0700

    Fixing Picking IDS copy on replace tax

--- sbm_inherit/inherit.py
+++ sbm_inherit/inherit.py
@@ -576,18 +576,38 @@ class account_invoice(osv.osv):
 					res[inv.id]+=line.price_subtotal
 
 		return res
+
+
 	def action_to_tax_replacement(self,cr,uid,ids,context={}):
 		res = False
 		invs = self.browse(cr,uid,ids,context=context)
 		newids = []
 		for inv in invs:
-			newid = self.copy(cr,uid,inv.id,context=context)
+			picking_ids = [(6,0, [pick.id]) for pick in inv.picking_ids]
+			# res = [(0,0,self._im_line_preparation(cr,uid,line)) for line in request.lines if (line.qty-line.processed_item_qty) > 0]
+			invoice_lines =  [(0, 0, self.pool.get('account.invoice.line').copy_data(cr,uid,line.id,{'invoice_id':False},context=context)) for line in inv.invoice_line]
+
+			# print invoice_lines,'----------------------'
+
+			# newid = self.copy(cr,uid,inv.id,default=default,context=context)
+			# override copy new
+			copy_new = self.copy_data(cr, uid, inv.id, context=context, default={'invoice_line':invoice_lines,'picking_ids':picking_ids})
+			
+			newid = self.create(cr,uid,copy_new,context=context)
+			# print newid$, "NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+			# raise osv.except_osv(_('Error!'),_('Tes'))
 			newids = [newid]
 			newInv = self.browse(cr,uid,newid,context=context)
-			fp_no = newInv.faktur_pajak_no[0:3]+'1'+newInv.faktur_pajak_no[4:20]
-			print fp_no,"8**************************"
+			print newInv,"NEW"
+			fp_no = newInv.faktur_pajak_no[0:2]+'1.'+newInv.faktur_pajak_no[4:20]
+
+			print fp_no,"FP**************************"
+			# raise osv.except_osv(_('Error!'),_('Tes'))
 			# self.write(cr,uid,newid,{'faktur_pajak_no':fp_no})
+			# canceling old invoice
 			self.write(cr,uid,[inv.id],{'state':'cancel','tax_invoice_origin_id':inv.id})
+			# new faktur  number
+			self.write(cr,uid,newid,{'faktur_pajak_no':fp_no})
 
 		mod_obj = self.pool.get('ir.model.data')
 		res = mod_obj.get_object_reference(cr, uid, 'account', 'invoice_form')
