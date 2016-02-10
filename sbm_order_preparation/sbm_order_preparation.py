43d7db67d3b84d88832ac0c86aa386bc29b664a8
ekachandrasetiawan <ekachandrasetiawan@yahoo.com>
2016-02-09 22:10:46 -0800

    OP Tambah Location dan Validasi Confirm Cek Lokasi Line & Lokasi OP

--- sbm_order_preparation/sbm_order_preparation.py
+++ sbm_order_preparation/sbm_order_preparation.py
@@ -16,6 +16,9 @@ class order_preparation(osv.osv):
 		'name': fields.char('Reference', required=True, size=64, select=True, readonly=True, states={'draft': [('readonly', False)]}),
 		'sale_id': fields.many2one('sale.order', 'Sale Order', select=True, required=True, readonly=True, domain=[('quotation_state','=', 'win')], states={'draft': [('readonly', False)]}),
 		'picking_id': fields.many2one('stock.picking', 'Delivery Order', required=False, domain="[('sale_id','=', sale_id), ('state','not in', ('cancel','done'))]", readonly=True, states={'draft': [('readonly', False)]},track_visibility='always'),
+		'duedate' : fields.date('Delivery Date', readonly=True, states={'draft': [('readonly', False)]},track_visibility='onchange'),
+		'location_id':fields.many2one('stock.location',required=True,string='Product Location',readonly=True, states={'draft': [('readonly', False)]}),
+
 	}
 
 	def sale_change(self, cr, uid, ids, sale):
@@ -28,10 +31,55 @@ class order_preparation(osv.osv):
 			res['duedate'] = data.delivery_date
 			res['partner_shipping_id'] = data.partner_shipping_id.id
 
+			location = []
 			for x in data.order_line:
 				material_lines=self.pool.get('sale.order.material.line').search(cr,uid,[('sale_order_line_id', '=' ,x.id)])
 				for y in self.pool.get('sale.order.material.line').browse(cr, uid, material_lines):
 
+					# Cek Material Line Dengan OP Line
+					nilai= 0
+					op_line=self.pool.get('order.preparation.line').search(cr,uid,[('sale_line_material_id', '=' ,y.id)])
+					for l in self.pool.get('order.preparation.line').browse(cr, uid, op_line):
+
+						# Cek Status OP 
+						op=self.pool.get('order.preparation').browse(cr, uid, [l.preparation_id.id])[0]
+						if op.state <> 'cancel':
+							nilai += l.product_qty
+
+
+
+					if nilai < y.qty:
+
+						location += [y.picking_location.id]
+
+						line.append({
+									 'product_id' : y.product_id.id,
+									 'product_qty': y.qty - nilai,
+									 'product_uom': y.uom.id,
+									 'name': y.desc,
+									 'sale_line_material_id':y.id
+						})
+			res['prepare_lines'] = line
+			return  {'value': res,'domain': {'location_id': [('id','in',tuple(location))]}}
+
+
+	def loc_change(self, cr, uid, ids, sale, loc):
+
+		if sale:
+			res = {}; line = []
+			data = self.pool.get('sale.order').browse(cr, uid, sale)
+			
+			res['poc'] = data.client_order_ref
+			res['partner_id'] = data.partner_id.id
+			res['duedate'] = data.delivery_date
+			res['partner_shipping_id'] = data.partner_shipping_id.id
+
+		
+			for x in data.order_line:
+				material_lines=self.pool.get('sale.order.material.line').search(cr,uid,[('sale_order_line_id', '=' ,x.id), ('picking_location', '=' , loc)])
+
+				for y in self.pool.get('sale.order.material.line').browse(cr, uid, material_lines):
+					
 					# Cek Material Line Dengan OP Line
 					nilai= 0
 					op_line=self.pool.get('order.preparation.line').search(cr,uid,[('sale_line_material_id', '=' ,y.id)])
@@ -57,8 +105,8 @@ class order_preparation(osv.osv):
 
 	def preparation_confirm(self, cr, uid, ids, context=None):
 		val = self.browse(cr, uid, ids)[0]
-		for x in val.prepare_lines:
 
+		for x in val.prepare_lines:
 			nilai= 0
 			op_line=self.pool.get('order.preparation.line').search(cr,uid,[('sale_line_material_id', '=' ,x.sale_line_material_id.id)])
 
@@ -67,7 +115,11 @@ class order_preparation(osv.osv):
 				op=self.pool.get('order.preparation').browse(cr, uid, [l.preparation_id.id])[0]
 				if op.state <> 'cancel':
 					nilai += l.product_qty
+
 			if x.sale_line_material_id.id:
+				if val.location_id.id <> x.sale_line_material_id.picking_location.id:
+					raise openerp.exceptions.Warning("Product Location Tidak Sama")
+
 				so_material_line=self.pool.get('sale.order.material.line').browse(cr, uid, [x.sale_line_material_id.id])[0]
 				mm = ' ' + so_material_line.product_id.default_code + ' '
 				msg = 'Product' + mm + 'Melebihi Order.!\n'
