b4efbd9a0b0336e83f68638a51d8d30a98529938
ekachandrasetiawan <ekachandrasetiawan@yahoo.com>
2016-02-10 22:43:57 -0800

    Location On Change & Approve OP, Check Data Product yang di Return

--- sbm_order_preparation/sbm_order_preparation.py
+++ sbm_order_preparation/sbm_order_preparation.py
@@ -42,25 +42,20 @@ class order_preparation(osv.osv):
 			for x in data.order_line:
 				material_lines=so_material_line.search(cr,uid,[('sale_order_line_id', '=' ,x.id)])
 				for y in so_material_line.browse(cr, uid, material_lines):
-
 					# Cek Material Line Dengan OP Line
 					nilai= 0
 					op_line = obj_op_line.search(cr,uid,[('sale_line_material_id', '=' ,y.id)])
 					for l in obj_op_line.browse(cr, uid, op_line):
-
 						# Cek Status OP 
 						op=obj_op.browse(cr, uid, [l.preparation_id.id])[0]
-
-						search_dn_lm=obj_dn_line_mat.search(cr, uid, [('op_line_id', '=' , [l.id])])
-
-						search_cek_return=obj_dn_line_mat_ret.search(cr, uid, [('delivery_note_line_material_id', '=' , [search_dn_lm])])
-
-						# Cek DN Line Material Return
 						product_return = 0
-
-						for rn in obj_dn_line_mat_ret.browse(cr, uid, search_cek_return):
-							if rn.stock_move_id.state == 'done':
-								product_return += rn.stock_move_id.product_qty
+						search_dn_lm=obj_dn_line_mat.search(cr, uid, [('op_line_id', '=' , [l.id])])
+						if search_dn_lm:
+							search_cek_return=obj_dn_line_mat_ret.search(cr, uid, [('delivery_note_line_material_id', '=' , [search_dn_lm])])
+							# Cek DN Line Material Return
+							for rn in obj_dn_line_mat_ret.browse(cr, uid, search_cek_return):
+								if rn.stock_move_id.state == 'done':
+									product_return += rn.stock_move_id.product_qty
 
 						if op.state <> 'cancel':
 							nilai += l.product_qty - product_return
@@ -80,6 +75,13 @@ class order_preparation(osv.osv):
 
 
 	def loc_change(self, cr, uid, ids, sale, loc):
+		so_material_line = self.pool.get('sale.order.material.line')
+		obj_op_line = self.pool.get('order.preparation.line')
+		obj_op = self.pool.get('order.preparation')
+		obj_dn_line_mat = self.pool.get('delivery.note.line.material')
+		obj_dn_line_mat_ret = self.pool.get('delivery.note.line.material.return')
+		obj_move = self.pool.get('stock.move')
+
 		if sale:
 			res = {}; line = []
 			data = self.pool.get('sale.order').browse(cr, uid, sale)
@@ -91,19 +93,31 @@ class order_preparation(osv.osv):
 
 		
 			for x in data.order_line:
-				material_lines=self.pool.get('sale.order.material.line').search(cr,uid,[('sale_order_line_id', '=' ,x.id), ('picking_location', '=' , loc)])
+				material_lines=so_material_line.search(cr,uid,[('sale_order_line_id', '=' ,x.id), ('picking_location', '=' , loc)])
 
-				for y in self.pool.get('sale.order.material.line').browse(cr, uid, material_lines):
+				for y in so_material_line.browse(cr, uid, material_lines):
 					
 					# Cek Material Line Dengan OP Line
 					nilai= 0
-					op_line=self.pool.get('order.preparation.line').search(cr,uid,[('sale_line_material_id', '=' ,y.id)])
-					for l in self.pool.get('order.preparation.line').browse(cr, uid, op_line):
+					op_line=obj_op_line.search(cr,uid,[('sale_line_material_id', '=' ,y.id)])
+					for l in obj_op_line.browse(cr, uid, op_line):
 
 						# Cek Status OP 
-						op=self.pool.get('order.preparation').browse(cr, uid, [l.preparation_id.id])[0]
+						op=obj_op.browse(cr, uid, [l.preparation_id.id])[0]
+
+						product_return = 0
+						search_dn_lm=obj_dn_line_mat.search(cr, uid, [('op_line_id', '=' , [l.id])])
+						if search_dn_lm:
+							search_cek_return=obj_dn_line_mat_ret.search(cr, uid, [('delivery_note_line_material_id', '=' , [search_dn_lm])])
+
+							# Cek DN Line Material Return
+							
+							for rn in obj_dn_line_mat_ret.browse(cr, uid, search_cek_return):
+								if rn.stock_move_id.state == 'done':
+									product_return += rn.stock_move_id.product_qty
+
 						if op.state <> 'cancel':
-							nilai += l.product_qty
+							nilai += l.product_qty - product_return
 
 					if nilai < y.qty:
 						line.append({
@@ -121,15 +135,30 @@ class order_preparation(osv.osv):
 	def preparation_confirm(self, cr, uid, ids, context=None):
 		val = self.browse(cr, uid, ids)[0]
 
+		obj_op_line = self.pool.get('order.preparation.line')
+		obj_op = self.pool.get('order.preparation')
+		obj_dn_line_mat = self.pool.get('delivery.note.line.material')
+		obj_dn_line_mat_ret = self.pool.get('delivery.note.line.material.return')
+		obj_move = self.pool.get('stock.move')
+
 		for x in val.prepare_lines:
 			nilai= 0
-			op_line=self.pool.get('order.preparation.line').search(cr,uid,[('sale_line_material_id', '=' ,x.sale_line_material_id.id)])
+			op_line=obj_op_line.search(cr,uid,[('sale_line_material_id', '=' ,x.sale_line_material_id.id)])
+			
+			for l in obj_op_line.browse(cr, uid, op_line):
+				op=obj_op.browse(cr, uid, [l.preparation_id.id])[0]
 
-			for l in self.pool.get('order.preparation.line').browse(cr, uid, op_line):
+				product_return = 0
+				search_dn_lm=obj_dn_line_mat.search(cr, uid, [('op_line_id', '=' , [l.id])])
+				if search_dn_lm:
+					search_cek_return=obj_dn_line_mat_ret.search(cr, uid, [('delivery_note_line_material_id', '=' , [search_dn_lm])])
+					# Cek DN Line Material Return
+					for rn in obj_dn_line_mat_ret.browse(cr, uid, search_cek_return):
+						if rn.stock_move_id.state == 'done':
+							product_return += rn.stock_move_id.product_qty
 
-				op=self.pool.get('order.preparation').browse(cr, uid, [l.preparation_id.id])[0]
 				if op.state <> 'cancel':
-					nilai += l.product_qty
+					nilai += l.product_qty - product_return
 
 			if x.sale_line_material_id.id:
 				if val.location_id.id <> x.sale_line_material_id.picking_location.id:
