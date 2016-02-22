b2ff85996572e352b7774c18bb900ab9a9dfc2c2
ekachandrasetiawan <ekachandrasetiawan@yahoo.com>
2016-02-22 02:09:19 -0800

    Class & View ADHOC Order Requisition & Work Order #59

---
+++ sbm_work_order/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    "name": "SBM WORK ORDER",
+    "version": "1.0",
+    "depends": ["base","mrp","sale","product","stock"],
+    "author": "PT.SUPRABAKTI MANDIRI",
+    "category": "WORK ORDER",
+    "description": """Modul ini digunakan untuk Menggolah SPK PT.Suprabakti Mandiri""",
+    "init_xml": [],
+    'update_xml': ["work_order_view.xml","adhoc_order_request_view.xml"],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+}
