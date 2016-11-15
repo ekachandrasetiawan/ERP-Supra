704cca6bc9773b523d479edba8ebf419a8da5229
ekachandrasetiawan <ekachandrasetiawan@yahoo.com>
2016-11-14 19:24:14 -0800

    #263 Flow Purchase Invoice

--- sbm_po_revise/__openerp__.py
+++ sbm_po_revise/__openerp__.py
@@ -1,7 +1,7 @@
 {
     "name": "Purchase Order Revise",
     "version": "1.0",
-    "depends": ["base","purchase","hr","product","sbm_purchase","sbm_purchaseorder","account","ad_purchase_import","purchase_partial_invoicing"],
+    "depends": ["base","purchase","hr","product","sbm_purchase","sbm_purchaseorder","account","ad_purchase_import","purchase_partial_invoicing","sbm_invoice_main_currency"],
     "author": "Suprabakti Mandiri",
     "category": "Purchase Order",
     "description": """Modul ini digunakan untuk Menghandel Purchase order Revisi atau Pengganti""",
