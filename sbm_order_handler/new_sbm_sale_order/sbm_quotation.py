14ee6058e3d000c9fb9190eb3693ba7f1660f05a
ekachandrasetiawan <ekachandrasetiawan@yahoo.com>
2016-12-22 00:08:07 -0800

    #307 Fitur onchange material Line

--- sbm_order_handler/new_sbm_sale_order/sbm_quotation.py
+++ sbm_order_handler/new_sbm_sale_order/sbm_quotation.py
@@ -662,10 +662,12 @@ class sale_order_material_line(osv.osv):
 		res={}
 		if product_id:
 			product= self.pool.get('product.product').browse(cr,uid,product_id,{})
-			res["value"]={
-			"uom":product.uom_id.id
-			}
 
+			if product.bom_ids:
+				return {'warning': {"title": _("Perhatian"), "message": _("Product Set Harus Dijabarkan")}, 'value': {'product_id': False}}
+
+			product= self.pool.get('product.product').browse(cr,uid,product_id,{})
+			res["value"]={"uom":product.uom_id.id}
 		return res
 
 	def onchange_product_uom(self,cr,uid,ids,product_id,uom,context={}):
