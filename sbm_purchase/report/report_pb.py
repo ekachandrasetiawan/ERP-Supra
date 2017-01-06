4cc04e986ebf6e3ef32ebfe0df4b3e77ece8e997
La Jayuhni Yarsyah <lajayuhni@gmail.com>
2017-01-05 17:32:06 -0800

    Print Out PB - perbaikan template

--- sbm_purchase/report/report_pb.py
+++ sbm_purchase/report/report_pb.py
@@ -4,36 +4,44 @@ from osv import osv
 import pooler
 
 class ReportPB(report_sxw.rml_parse):
-    def __init__(self, cr, uid, name, context):
-        super(ReportPB, self).__init__(cr, uid, name, context=context)
-        self.localcontext.update({
-            'time': time,
-            'sum' : sum,
-            'get_lines':self.get_lines,
-            })
-    def get_lines(self,obj):
-        detail_pb_ids= obj.detail_pb_ids
-        res=[]
-        arrLine={}
-        i=1
-        # customer= ''
-        for x in detail_pb_ids:
+	def __init__(self, cr, uid, name, context):
+		super(ReportPB, self).__init__(cr, uid, name, context=context)
+		self.localcontext.update({
+			'time': time,
+			'sum' : sum,
+			'get_lines':self.get_lines,
+			})
+	def get_lines(self,obj):
+		detail_pb_ids= obj.detail_pb_ids
+		res=[]
+		arrLine={}
+		i=1
+		# customer= ''
+		for x in detail_pb_ids:
 
-            if x.variants.id==False:
-                name = x.name.name
-            else:
-                name = x.variants.name
+			if x.variants.id==False:
+				name = x.name.name
+			else:
+				name = x.variants.name
 
-            so_name = False
-            if x.sale_line_ids.id:
-                so_name = x.sale_line_ids.order_id.name
-                
-            
+			so_name = False
+			if x.sale_line_ids.id:
+				so_name = x.sale_line_ids.order_id.name
 
-            arrLine.update({'no':i,'name':name,'satuan':x.satuan,'part_no':x.part_no,'jumlah_diminta':x.jumlah_diminta,'stok':x.stok,'keterangan':x.keterangan,'so_name':so_name})
-            res.append(arrLine)
-            arrLine={}
-            i+=1
-        return res
+			part_no = x.part_no
+			if x.detail_pb_id.proc_type=='sales':
+				part_no = x.name.default_code
+
+
+			keterangan =  x.keterangan
+			if len(x.keterangan)>64:
+				name += "\r\n\r\n"+x.keterangan
+				keterangan = ""
+
+			arrLine.update({'no':i,'name':name,'satuan':x.satuan,'part_no':part_no,'jumlah_diminta':x.jumlah_diminta,'stok':x.stok,'keterangan':keterangan,'so_name':so_name})
+			res.append(arrLine)
+			arrLine={}
+			i+=1
+		return res
 
 report_sxw.report_sxw('report.print.pb', 'pembelian.barang', 'addons/sbm_purchase/report/report_pb.rml', parser = ReportPB, header = False)
