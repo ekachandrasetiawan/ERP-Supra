3e63f09d1ec60d354d6d889031e0ce2962b7c62a
ikhsanputra07 <ikhsan.madrid@gmail.com>
2017-01-10 20:32:56 -0800

    menambahkan modul document changes request

---
+++ sbm_document_changes_request/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Document Changes Request",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Suprabakti Mandiri",
+    "category": "Document Changes Request",
+    "description": """Tester""",
+    "init_xml": [],
+    'update_xml': ["document_changes_request_view.xml"],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
---
+++ sbm_document_changes_request/document_changes_request.py
@@ -0,0 +1,64 @@
+import time
+from datetime import date, timedelta, datetime
+import netsvc
+from tools.translate import _
+from osv import osv, fields
+
+class document_changes_request(osv.osv):
+
+	_name = 'document.changes.request'
+	_columns = {
+		'name': fields.char('Name'),
+		'request_id': fields.many2one('res.users', "Request Name",required=True),
+		'reasons': fields.text('Reasons',required=True),
+		'document_model': fields.many2one('ir.model', "Document Model"),
+		'fk_model': fields.integer('Fk Model'),
+		'document_name' : fields.char('Document Name'),
+		'fault_category' : fields.selection([('user', 'User Fault'), ('external', 'External Fault'), ('system', 'System Fault')], 'Fault Category', select=True),
+		'it_notes' : fields.text('IT Notes',required=False),
+		'state' : fields.selection([('draft', 'Draft'), ('submitted', 'Submitted'), ('approved', 'Approved'), ('canceled', 'Canceled'), ('rejected', 'Rejected'), ('done', 'Done')], 'State', select=True),
+	}
+	
+
+	def action_submit(self, cr, uid, ids, context=None):
+
+		return self.write(cr,uid,ids,{'state':'submitted'},context=context)
+
+	def action_approve(self, cr, uid, ids, context=None):
+
+		return self.write(cr,uid,ids,{'state':'approved'},context=context)
+
+	def action_reject(self, cr, uid, ids, context=None):
+
+		return self.write(cr,uid,ids,{'state':'rejected'},context=context)
+
+	def action_validate(self, cr, uid, ids, context=None):
+
+		return self.write(cr,uid,ids,{'state':'done'},context=context)
+
+	def action_open_state(self, cr, uid, ids, context=None):
+
+		return self.write(cr,uid,ids,{'state':'draft'},context=context)
+
+	def _request_get(self, cr, uid, context=None):
+		obj_user = self.pool.get('res.users')
+
+		cek = obj_user.search(cr,uid,[('id', '=' ,uid)])
+
+		if cek:
+			# hasil=obj_user.browse(cr,uid,cek)[0]
+			return uid
+		else:
+			return False	
+
+	_defaults = {
+		'state': 'draft',
+		'request_id': _request_get,
+	}
+
+	def reqId(self, cr, uid, ids, request_id):
+		if uid != request_id:
+			return {'warning': {"title": _("Perhatian"), "message": _("User Harus yang login")}, 'value': {'request_id': uid, 'fault_category': 'user'}}
+		return True	
+
+document_changes_request()
